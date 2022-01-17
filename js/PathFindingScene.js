5253f0c182021a12962759df3b3615221de4fdf3
mxhdsn <maxhud19@gmail.com>
2022-01-17 12:32:21 +0000

    Monday Morning
    Player Movement
    Gun Added
    Firing

--- js/PathFindingScene.js
+++ js/PathFindingScene.js
@@ -21,16 +21,47 @@ class PathFindingScene extends Phaser.Scene {
     }
     
     preload() {
+        //-- Tilemap and Tileset --//
         this.load.image('tileset', 'assets/tiles100-spacing2.png')
         this.load.tilemapTiledJSON('level1', 'assets/level1.json')
+        //-- Player --//
+        this.load.image('man', 'assets/man.png')
+        this.load.image('man-with-gun', 'assets/man-with-gun.png')
+        //-- Gun --//
+        this.load.image('gun', 'assets/gun.png')
+        this.load.image('bullet', 'assets/bullet.png')
     }
 
     create() {
+        //-- Creating Map --//
         this.map = this.make.tilemap({ key: 'level1' })
         this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels)
         this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels)
         const tileset = this.map.addTilesetImage('tileset', 'tileset')
         const groundAndWallsLayer = this.map.createLayer('groundAndWallsLayer', 'tileset', 0, 0)
+        groundAndWallsLayer.setCollisionByProperty({ valid:false })
+        const objectLayer = this.map.getObjectLayer('objectLayer')
+        //-- Creating Player --//
+        objectLayer.objects.forEach(function(object){
+            let dataObject = Utils.RetrieveCustomProperties(object)
+            if(dataObject.type === 'playerSpawn') {
+                this.player = new Player(this, dataObject.x, dataObject.y, 'man')
+            } else if(dataObject.type === 'gunSpawn') {
+                // @ts-ignore
+                this.gun = this.physics.add.sprite(dataObject.x, dataObject.y, 'gun')
+            }
+        }, this)
+        this.physics.add.collider(this.player.sprite, groundAndWallsLayer)
+        this.physics.add.overlap(this.player.sprite, this.gun, this.collectGun, null, this)
+        //-- Bullet Group --//
+        this.bullets = this.physics.add.group({
+            defaultKey: 'bullet',
+            maxSize: 5,
+            collideWorldBounds: true
+        })
+        this.physics.world.on('worldbounds', this.worldBoundsBullet, this)
+        this.physics.add.collider(this.bullets, groundAndWallsLayer, this.bulletHitWall, null, this)
+        this.events.on('firebullet', this.fireBullet, this)
     }
 
     findPath(point) {
@@ -46,15 +77,30 @@ class PathFindingScene extends Phaser.Scene {
     }
 
     collectGun(player, gun) {
+        this.gun.destroy()
+        this.player.hasGun = true
+        this.player.sprite.setTexture('man-with-gun')
     }
     
     fireBullet() {
+        let bullet = this.bullets.get(this.player.sprite.x, this.player.sprite.y)
+        if(bullet){
+            bullet.setDepth(3)
+            bullet.body.collideWorldBounds = true
+            bullet.body.onWorldBounds = true
+            bullet.enableBody(false, bullet.x, bullet.y, true, true)
+            bullet.rotation = this.player.sprite.rotation
+            this.physics.velocityFromRotation(bullet.rotation, 1000, bullet.body.velocity)
+        }
     }
 
     worldBoundsBullet(body) {
+        //-- Return Bullet To the Pool --//
+        body.gameObject.disableBody(true, true)
     }
 
     bulletHitWall(bullet, layer) {
+        bullet.disableBody(true, true)
     }
 
     bulletHitEnemy(enemySprite, bullet) {
@@ -64,5 +110,6 @@ class PathFindingScene extends Phaser.Scene {
     }
 
     update(time, delta) {
+        this.player.update(time, delta)
     }
 }
--- js/Player.js
+++ js/Player.js
@@ -19,8 +19,48 @@ class Player {
     hasGun = false
 
     constructor(scene, x, y, texture) {
+        this.scene = scene
+        this.sprite = scene.physics.add.sprite(x, y, texture)
+        this.sprite.setDepth(2)
+        this.sprite.setCollideWorldBounds(true)
+        this.spaceBar = scene.input.keyboard.createCursorKeys().space
+        this.keys = scene.input.keyboard.addKeys({
+            w: Phaser.Input.Keyboard.KeyCodes.W,
+            a: Phaser.Input.Keyboard.KeyCodes.A,
+            s: Phaser.Input.Keyboard.KeyCodes.S,
+            d: Phaser.Input.Keyboard.KeyCodes.D
+        })
+        this.currentSpeed = this.standardSpeed
     }
     
     update(time, delta) {
+        //-- Player Movement --//
+        if(!this.isDead){
+            if (this.keys.a.isDown){
+                this.sprite.setVelocity(-this.currentSpeed, 0)
+                this.sprite.angle = 180
+            } else if (this.keys.d.isDown){
+                this.sprite.setVelocity(this.currentSpeed, 0)
+                this.sprite.angle = 0
+            } else if (this.keys.w.isDown){
+                this.sprite.setVelocity(0, -this.currentSpeed)
+                this.sprite.angle = 270
+            } else if (this.keys.s.isDown){
+                this.sprite.setVelocity(0, this.currentSpeed)
+                this.sprite.angle = 90
+            } else {
+                this.sprite.setVelocity(0, 0)
+            }
+            //-- Resize Hitbox Depending on Rotation --//
+            if (this.sprite.body.velocity.x !=0 ) {
+                this.sprite.setSize(this.sprite.width, this.sprite.height)
+            } else if (this.sprite.body.velocity.y !=0) {
+                this.sprite.setSize(this.sprite.height, this.sprite.width)
+            }
+            //-- Firing Bullets --//
+            if(this.hasGun && Phaser.Input.Keyboard.JustDown(this.spaceBar)){
+                this.scene.events.emit('firebullet')
+            }
+        }
     }
 }
